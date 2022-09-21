9ae7b4bcfd51058c279f5cd1bd5621c5f9038f7b
rupasree <rupasree124@gmail.com>
2022-09-21 18:21:54 +0530

    new files

--- src/App.js
+++ src/App.js
@@ -1,82 +1,16 @@
 import JobOpenings from "./components/JobOpenings";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useCallback } from "react";
+import {Routes , Route , BrowserRouter} from "react-router-dom"
+import SingleJobListing from "./components/Listing/SingleJobListing";
 
 function App() {
-  // const fetchJobDetailsHandler = async () => {
-  //   let url = "https://lucidatechnologies-team.freshteam.com/api/job_postings";
-  //   try {
-  //     const response = await fetch(url, {
-  //       method: "GET",
-  //       headers: {
-  //         Authorization: `Bearer fyLvFPNHSQUtxJX2sPsq7g`,
-  //         "Content-Type": "application/json ",
-  //       },
-  //     });
-  //     if (!response.ok) {
-  //       throw new Error("Something went wrong!");
-  //     }
-
-  //     const data = await response.json();
-
-  //     console.log(data);
-
-  //     const loadedData = [];
-
-  //     for (const key in data) {
-  //       loadedData.push({
-  //         id: data[key].id,
-  //         title: data[key].title,
-  //       });
-  //     }
-
-  //     console.log(loadedData);
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchJobDetailsHandler();
-  // }, []);
-
-  const fetchJobDetailsHandler = useCallback(async () => {
-    let url = "https://lucidatechnologies-team.freshteam.com/api/job_postings";
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer fyLvFPNHSQUtxJX2sPsq7g`,
-          "Content-Type": "application/json ",
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Something went wrong!");
-      }
-
-      const data = await response.json();
-
-      const loadedJobData = [];
-
-      for (const key in data) {
-        loadedJobData.push({
-          id: key,
-          title: data[key].title,
-          experience: data[key].experience,
-          description: data[key].description,
-        });
-      }
-      console.log(loadedJobData);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchJobDetailsHandler();
-  }, [fetchJobDetailsHandler]);
-
-  return <JobOpenings />;
+  return(
+      <Routes>
+        <Route path="/" element={<JobOpenings/>}/>
+        <Route path ="/job/:id" element={<SingleJobListing/>}/>
+      </Routes>
+  )
 }
 
 export default App;
